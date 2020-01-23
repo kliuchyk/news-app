6cff05a10492a80ebdb85f3760e7544024a3ee6d
kliuchyk <kliuchyk@gmail.com>
2020-01-23 12:15:24 +0200

    add login page structure

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
 export default function Login() {
-  return <h1>Login Page</h1>;
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">User Name</label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Log in
+      </button>
+    </form>
+  );
 }
