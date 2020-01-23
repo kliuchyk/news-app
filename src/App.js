e34dfc69437c2b86317516bef32cdcdddc7faed8
kliuchyk <kliuchyk@gmail.com>
2020-01-23 10:57:49 +0200

    add basic app structure and navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './containers/Login';
+import Profile from './containers/Profile';
+import NewsList from './containers/NewsList';
+import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/news" component={NewsList} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ src/containers/NewsList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewsList() {
+  return <h1>News List Page</h1>;
+}
