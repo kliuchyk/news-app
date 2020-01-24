4cc3f08dcb8e7554608dc7c2ecb3de9628f346cb
kliuchyk <kliuchyk@gmail.com>
2020-01-24 12:17:01 +0200

    rename components

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Header() {
+export default function AppHeader() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
