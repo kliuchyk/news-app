7bf36f4bced1edb644d748352c8bc3da939c6aef
kliuchyk <kliuchyk@gmail.com>
2020-01-24 12:20:09 +0200

    add app routes

---
+++ src/routes/NewsPage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import NewsList from '../containers/NewsList';
+
+export default function NewsPage() {
+  return <NewsList />;
+}
---
+++ src/routes/ProfilePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ProfileDetails from '../containers/ProfileDetails';
+
+export default function ProfilePage() {
+  return <ProfileDetails />;
+}
