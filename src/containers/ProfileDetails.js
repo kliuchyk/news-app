38820d4658a1ae56e898c6cbf34067d3aa4fd28e
kliuchyk <kliuchyk@gmail.com>
2020-01-26 13:56:34 +0200

    render user details page

--- src/containers/ProfileDetails.js
+++ src/containers/ProfileDetails.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Jumbotron, Container } from 'react-bootstrap';
 
-export default function ProfileDetails() {
-  return <h1>Profile Page</h1>;
+function ProfileDetails({ name }) {
+  return (
+    <div>
+      <h1 className="text-center p-3">Profile Details</h1>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Hello {name}!</h1>
+          <p>
+            This page will be modified in the nearest future so the user will be
+            able to edit some profile data.
+          </p>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
 }
+
+const mapStateToProps = state => {
+  return {
+    name: state.user.name
+  };
+};
+
+export default connect(mapStateToProps)(ProfileDetails);
